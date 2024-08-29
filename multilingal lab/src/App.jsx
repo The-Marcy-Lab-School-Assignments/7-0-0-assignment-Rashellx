7a3b91b5f28363cb8437ce8a9828a9ac80867d4d
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-08-29 15:23:02 -0400

    Done

--- multilingal lab/src/App.jsx
+++ multilingal lab/src/App.jsx
@@ -5,18 +5,19 @@ import './App.css'
 
 function App() {
   const [language, setLanguage] = useState("Good Morning")
+  const [fontSize, setFontSize] = useState(45)
 
 
   return (
     <>
-      <button>-</button>
-      <button>+</button>
-      <h1>{language}</h1>
+      <button onClick={() => setFontSize(fontSize - 2)}>-</button>
+      <button onClick={() => setFontSize(fontSize + 2)}>+</button >
+      <h1 style={{ fontSize }}>{language}</h1>
       <button onClick={() => setLanguage("Good Morning")}>English</button>
-      <button onClick={() => setLanguage("Ggvbhj")}>Chinese</button>
-      <button>Spanish</button>
-      <button>Haitian</button>
-      <button>Portugues</button>
+      <button onClick={() => setLanguage("早上好")}>Chinese</button>
+      <button onClick={() => setLanguage("¡Buenos días!")}>Spanish</button>
+      <button onClick={() => setLanguage("Bon maten")}>Haitian</button>
+      <button onClick={() => setLanguage("Bom dia")}>Portugues</button>
     </>
   )
 }
